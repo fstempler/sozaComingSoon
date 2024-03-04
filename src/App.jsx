79c3cd7a380b0e18bcbe8a0ff2739de77117e761
fstempler <zackmango@gmail.com>
2024-03-04 10:21:25 -0300

    Site Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import Logo from '../public/LogoSoza.png';
 
-import './App.css'
+import './App.css';
 
 function App() {
   
 
   return (
     <>
-      
+      <div className='mainContainer'>
+        <div className='navbar'>
+          <img src={Logo} alt="Soza Logo" id='logo' className='Logo'/>
+          <p>Innovate. Construct. Transform</p>
+        </div>
+        
+        <section className='heroContainer'>
+          <div className='heroTitleContainer'>
+            <h1 className='heroTitle'>
+              Coming soon.
+            </h1>
+          </div>        
+            
+          <div className='heroFooter'>
+            <h3 className='email'>sozaprojects.com.ar</h3>
+          </div>
+        </section>
+      </div>
     </>
   )
 }
